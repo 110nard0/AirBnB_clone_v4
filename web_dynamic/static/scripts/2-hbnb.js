49ace73d813d50b11c46eb536f4f14f9527bd44c
psych0wh1z <ikeagudike@gmail.com>
2023-05-05 21:54:17 +0100

    Add new hbnb route

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  const amenities = {};
+
+  $("li input[type='checkbox']").change(function () {
+    const amenityName = $(this).data('name');
+    const amenityId = $(this).data('id');
+
+    if (this.checked) {
+      amenities[amenityName] = amenityId;
+    } else {
+      delete amenities[amenityName];
+    }
+
+    const amenitiesList = Object.keys(amenities).sort().join(', ');
+    $('div.amenities h4').text(amenitiesList);
+  });
+ 
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/',
+    function (data) {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    });
+});
