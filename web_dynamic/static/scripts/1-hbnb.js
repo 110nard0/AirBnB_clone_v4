d146ea8453516bf516222a045f88099ab9469d60
Obihai <obichi@coseo.com>
2023-05-05 00:28:42 -0700

    Updated js file

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  const amenities = {};
+
+  $("li input[type='checkbox']").change(function () {
+    const amenityName = $(this).data('name');
+    const amenityId = $(this).data('id');
+
+    if (this.checked) {
+      amenities[amenityName] = amenityId;
+    } else {
+      delete amenities[amenityName];
+    }
+
+    const amenitiesList = Object.keys(amenities).sort().join(", ");
+    $(".amenities h4").text(amenitiesList);
+  });
+});
