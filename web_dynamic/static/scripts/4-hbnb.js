2af56bb15e36270111c956433a74f4f0008c99aa
psych0wh1z <ikeagudike@gmail.com>
2023-05-07 10:53:54 +0100

    Add new route for searching places by amenity

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+  const amenities = {};
+
+  $("li input[type='checkbox']").change(function () {
+    const amenityName = $(this).data('name');
+    const amenityId = $(this).data('id');
+
+    if (this.checked) {
+      amenities[amenityName] = amenityId;
+    } else {
+      delete amenities[amenityName];
+    }
+
+    const amenitiesList = Object.keys(amenities).sort().join(', ');
+    $('div.amenities h4').text(amenitiesList);
+  });
+
+  // get api status
+  // $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+  $.getJSON('http://127.0.0.1:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+
+  // fetch places through api request
+  $.post({
+    // url: 'http://0.0.0.0:5001/api/v1/places_search',
+    url: 'http://127.0.0.1:5001/api/v1/places_search',
+    data: JSON.stringify({}),
+    contentType: 'application/json',
+    success: (data) => {
+      data.forEach(place => {
+        const article =
+          '<article><div class="title_box"><h2>' +
+           place.name +
+          '</h2><div class="price_by_night">$' +
+          place.price_by_night +
+          '</div></div><div class="information"><div class="max_guest">' +
+          place.max_guest +
+          ' Guests</div><div class="number_rooms">' +
+          place.number_rooms +
+          ' Bedrooms</div><div class="number_bathrooms">' +
+          place.number_bathrooms +
+          'Bathrooms</div></div><div class="description">' +
+          place.description +
+          '</div></article>';
+        $('section.places').append(article);
+      });
+    }
+  });
+
+  // filter places by amenity
+  $('button').on('click', () => {
+    const amenityIDList = Object.values(amenities);
+    $.post({
+      // url: 'http://0.0.0.0:5001/api/v1/places_search',
+      url: 'http://127.0.0.1:5001/api/v1/places_search',
+      data: JSON.stringify({ amenities: amenityIDList }),
+      contentType: 'application/json',
+      success: (data) => {
+        $('section.places').empty();
+        data.forEach(place => {
+          const article =
+            '<article><div class="title_box"><h2>' +
+            place.name +
+            '</h2><div class="price_by_night">$' +
+            place.price_by_night +
+            '</div></div><div class="information"><div class="max_guest">' +
+            place.max_guest +
+            ' Guests</div><div class="number_rooms">' +
+            place.number_rooms +
+            ' Bedrooms</div><div class="number_bathrooms">' +
+            place.number_bathrooms +
+            'Bathrooms</div></div><div class="description">' +
+            place.description +
+            '</div></article>';
+          $('section.places').append(article);
+        });
+      }
+    });
+  });
+});
